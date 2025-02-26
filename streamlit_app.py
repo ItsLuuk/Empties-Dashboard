12a09c2ff0325da58fc090ab331f91c5ee91a3a5
ItsLuuk <145759853+ItsLuuk@users.noreply.github.com>
2025-02-26 11:40:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Empties Dashboard")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building"
 )
+
